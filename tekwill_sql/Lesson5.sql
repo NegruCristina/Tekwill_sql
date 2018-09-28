a8fdcc9380645e3fdda8db3c89af3b949f1ecdd7
negru.cristina.g <negru.cristina.g@gmail.com>
2018-09-28 19:01:43 +0300

    Lectia 5

---
+++ tekwill_sql/Lesson5.sql
@@ -0,0 +1,33 @@
+SELECT student_id; first_name; student_reg_year AS Admission
+FROM ad_student_details;
+
+
+
+SELECT *
+FROM ad_student_details;
+
+
+
+SELECT student_id
+,first_name
+,student_reg_year
+,student_reg_year+2 "Course Expire"
+FROM ad_student_details;
+
+
+Describe ad_course_details
+
+
+SELECT *
+From ad_course_details;       
+       
+    
+
+
+SELECT *
+From employees;
+
+
+SELECT first_name || ' ' ||last_name AS "Nume Prenume"
+From employees;
+       
