e2c0c4bd409168828c189fa52574016f0a293ab3
negru.cristina.g <negru.cristina.g@gmail.com>
2018-09-30 17:31:10 +0300

    home6+

--- tekwill_sql/Home6.sql
+++ tekwill_sql/Home6.sql
@@ -21,18 +21,19 @@ FROM AD_EXAM_RESULTS
 WHERE MARKS NOT BETWEEN 65 AND 90;
 
 
-
-
-
 SELECT student_id,
-       first_name,
-       student_reg_year
+      first_name,
+      student_reg_year
 FROM AD_STUDENT_DETAILS
-Where FIRST_NAME like'NINA' 
-AND FIRST_NAME like'ROBERT';
+WHERE first_name in ('ROBERT', 'NINA')
+order by student_id asc;
+      
 
-
-----
+SELECT course_name,
+      department_id
+FROM ad_course_details
+where department_id in ('20', '40')
+order by course_name asc;
 
 
 
@@ -60,3 +61,67 @@ FROM AD_STUDENT_DETAILS
 WHERE email_addr IS NULL;
 
 
+SELECT first_name,
+      student_reg_year,
+      email_addr
+FROM AD_STUDENT_DETAILS
+WHERE email_addr IS NOT NULL
+Order by 2 desc, 3 desc;
+
+
+
+SELECT student_id,
+       marks
+FROM ad_exam_results
+WHERE marks > &score;
+
+
+SELECT course_id,
+       course_name,
+       session_id
+FROM ad_course_details
+WHERE department_id =&dept_num
+ORDER BY course_name;
+
+
+SELECT course_id,
+       course_name,
+       session_id
+FROM AD_COURSE_DETAILS
+WHERE department_id=20
+order by course_id;
+
+Select course_id,
+       course_name,
+       session_id
+FROM AD_COURSE_DETAILS
+WHERE department_id =40
+order by session_id;
+
+
+
+SELECT first_name
+FROM AD_STUDENT_DETAILS
+WHERE FIRST_NAME LIKE '_O%';
+
+
+SELECT first_name
+FROM AD_STUDENT_DETAILS
+WHERE first_name like '%A%' 
+and first_name like '%N%';
+
+
+    
+SELECT COURSE_ID,
+       COURSE_NAME
+FROM AD_COURSE_DETAILS
+WHERE DEPARTMENT_ID IN ('10','20')
+AND SESSION_ID NOT IN ('200','200');
+
+
+SELECT student_id "Student #",
+       exam_id "Exam Code",
+       course_id "Course Code",
+       Marks "Code"
+FROM AD_EXAM_RESULTS
+Where marks = 70;
